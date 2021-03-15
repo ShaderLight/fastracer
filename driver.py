27e7ad910d8cb9d17cf2f50ce95668b56b920ff1
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2021-03-15 19:21:32 +0100

    Cleaning up
    Removed unused imports, changed logging

--- driver.py
+++ driver.py
@@ -1,10 +1,7 @@
 import logging
-from time import sleep
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
@@ -12,30 +9,25 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt=
 game_url = input("Enter game url: ")
 
 browser = Chrome()
-logging.info("Chromedriver initialised")
+logging.debug("Chromedriver initialised")
 
 browser.get(game_url)
-logging.info("Get request sent")
+logging.debug("Get request sent")
 
+logging.debug("Waiting for join button")
 race_button = WebDriverWait(browser, 10).until(ec.presence_of_element_located((By.CLASS_NAME, 'raceAgainLink')))
-logging.info("Waiting for join button")
-
-logging.info("Joining in 3")
-for i in range(2,-1,-1):
-    sleep(1)
-    logging.info(str(i))
 
 race_button.click()
 logging.info("Joining...")
 
-
-input("Press enter after game starts")
+input("Press enter after the game starts")
 
 text_container = WebDriverWait(browser, 10).until(ec.presence_of_element_located((By.CLASS_NAME, 'inputPanel')))
 game_text = text_container.text
-print(game_text)
+logging.debug("Text panel found, text scrapped")
 
 input_box = WebDriverWait(browser, 10).until(ec.presence_of_element_located((By.CLASS_NAME, 'txtInput')))
 
+logging.debug("Input panel detected, sending inputs...")
 for character in game_text:
     input_box.send_keys(character)
